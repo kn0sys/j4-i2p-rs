d9e5a1a1fd156f026c2dbf6089b7ed02cf6d47c8
kn0sys <nigel.christian@hiahatf.org>
2024-08-20 19:53:40 -0400

    add constant for getSk

--- README.md
+++ README.md
@@ -5,17 +5,32 @@ i2p rust wrapper
 
 ## Getting Started
 
-`git clone --recursive https://github.com/kn0sys/j4-i2p-rs`
+```bash
+git clone --recursive https://github.com/kn0sys/j4-i2p-rs
+```
 
 build the jars
 
-`cd i2p`
-`ant buildRouter buildI2PTunnelJars buildSAM jbigi buildAddressbook`
+```bash
+cd i2p
+```
+
+```bash
+ant buildRouter buildI2PTunnelJars buildSAM jbigi buildAddressbook
+```
 
 copy jars to the jassets directory
 
 
-`cd build`
-`mkidr ../../opt/j4-i2p-rs/jassets`
-`for i in addressbook.jar i2ptunnel.jar i2p.jar mstreaming.jar router.jar sam.jar streaming.jar; do cp "$i" "../../opt/j4-i2p-rs/jassets"; done`
+```bash
+cd build
+```
+
+```bash
+mkidr -p ../../opt/j4-i2p-rs/jassets
+```
+
+```bash
+for i in addressbook.jar i2ptunnel.jar i2p.jar mstreaming.jar router.jar sam.jar streaming.jar; do cp "$i" "../../opt/j4-i2p-rs/jassets"; done
+```
 
--- src/tunnel_control.rs
+++ src/tunnel_control.rs
@@ -1,14 +1,15 @@
 use j4rs::*;
 
-pub const BYTE_ARRAY_STREAM_CLASS: &str     = "java.io.ByteArrayOutputStream";
-pub const I2P_CLIENT_CLASS: &str            = "net.i2p.I2PClient";
-pub const I2P_CLIENT_FACTORY_CLASS: &str    = "net.i2p.client.I2PClientFactory";
-pub const I2P_DESTINATION_CLASS: &str       = "net.i2p.data.Destination";
-pub const BASE64_CLASS: &str                = "net.i2p.data.Base64";
-pub const METHOD_CREATE_DESTINATION: &str   = "createDestination";
-pub const METHOD_ENCODE: &str               = "encode";
-pub const METHOD_CREATE_CLIENT: &str        = "createClient";
-pub const METHOD_TO_BASE32: &str            = "toBase32";
+const BYTE_ARRAY_STREAM_CLASS: &str     = "java.io.ByteArrayOutputStream";
+const I2P_CLIENT_CLASS: &str            = "net.i2p.I2PClient";
+const I2P_CLIENT_FACTORY_CLASS: &str    = "net.i2p.client.I2PClientFactory";
+const I2P_DESTINATION_CLASS: &str       = "net.i2p.data.Destination";
+const BASE64_CLASS: &str                = "net.i2p.data.Base64";
+const METHOD_CREATE_DESTINATION: &str   = "createDestination";
+const METHOD_ENCODE: &str               = "encode";
+const METHOD_CREATE_CLIENT: &str        = "createClient";
+const METHOD_TO_BASE32: &str            = "toBase32";
+const METHOD_GET_SK: &str               = "getSk";
 
 /// Keypair contains the secret key `sk`
 ///
@@ -26,7 +27,7 @@ impl KeyPair {
         let sk_instance = jvm.create_instance(BYTE_ARRAY_STREAM_CLASS, InvocationArg::empty())?;
         let client = jvm.invoke_static(I2P_CLIENT_FACTORY_CLASS, METHOD_CREATE_CLIENT, InvocationArg::empty())?;
         let destination = jvm.invoke(&client, METHOD_CREATE_DESTINATION, &[&InvocationArg::from(sk_instance)])?;
-        let sk_bytes = jvm.invoke(&destination, "getSk", InvocationArg::empty())?;
+        let sk_bytes = jvm.invoke(&destination, METHOD_GET_SK, InvocationArg::empty())?;
         let b64 = jvm.invoke_static(BASE64_CLASS, METHOD_ENCODE, &[InvocationArg::from(sk_bytes)])?;
         let sk: String = jvm.to_rust(b64)?;
         let b32_dest_instance = jvm.invoke(&destination, METHOD_TO_BASE32, InvocationArg::empty())?;
