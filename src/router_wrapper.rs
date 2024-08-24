d437cff41bbd0305f8f46728996e61489dcb094c
kn0sys <nigel.christian@hiahatf.org>
2024-08-24 10:06:39 -0400

    add server tunnel

--- src/router_wrapper.rs
+++ src/router_wrapper.rs
@@ -1,25 +1,28 @@
 use j4rs::*;
 
-pub const ROUTER_CLASS: &str = "net.i2p.router.Router";
-pub const METHOD_RUN: &str = "runRouter";
+const BASE_PATH: &str           = "opt.j4-i2p-rs";
+const ROUTER_CLASS: &str        = "net.i2p.router.Router";
+pub const METHOD_RUN: &str      = "runRouter";
 pub const METHOD_IS_ALIVE: &str = "isAlive";
 pub const METHOD_SHUTDOWN: &str = "shutdownGracefully";
 
 /// Wrapper for net.i2p.router
-pub struct Wrapper;
+pub struct Wrapper {
+    router: Instance,
+}
 
 impl Wrapper {
     /// Return a new router instance.
-    pub fn create_router() -> Result<Instance, errors::J4RsError> {
+    pub fn create_router() -> Result<Self, errors::J4RsError> {
         log::info!("create_router");
-        let jvm = JvmBuilder::new().with_base_path("opt/j4-i2p-rs").build()?;
+        let jvm = JvmBuilder::new().with_base_path(BASE_PATH).build()?;
         let router = jvm.create_instance(ROUTER_CLASS, InvocationArg::empty())?;
-        Ok(router)
+        Ok(Wrapper {router})
     }
-    pub fn invoke_router(router: &Instance, method_name: &str) -> Result<(), errors::J4RsError> {
+    pub fn invoke_router(&self, method_name: &str) -> Result<(), errors::J4RsError> {
         log::info!("invoke_router::{}", method_name);
-        let jvm = JvmBuilder::new().with_base_path("opt/j4-i2p-rs").build()?;
-        let _ = jvm.invoke(&router, method_name, InvocationArg::empty())?;
+        let jvm = JvmBuilder::new().with_base_path(BASE_PATH).build()?;
+        let _ = jvm.invoke(&self.router, method_name, InvocationArg::empty())?;
         Ok(())
     }
 }
--- src/tunnel_control.rs
+++ src/tunnel_control.rs
@@ -1,6 +1,12 @@
 use j4rs::*;
+use rand::RngCore;
 
+const BASE_PATH: &str                   = "opt/j4-i2p-rs";
+const I2P_TUNNEL_CLASS: &str            = "net.i2p.i2ptunnel";
 const BYTE_ARRAY_STREAM_CLASS: &str     = "java.io.ByteArrayOutputStream";
+const PATH_CLASS: &str                  = "java.nio.file.PATH";
+const FILES_CLASS: &str                 = "java.nio.file.Files";
+const FILE_CLASS: &str                  = "java.io.File";
 const I2P_CLIENT_CLASS: &str            = "net.i2p.I2PClient";
 const I2P_CLIENT_FACTORY_CLASS: &str    = "net.i2p.client.I2PClientFactory";
 const I2P_DESTINATION_CLASS: &str       = "net.i2p.data.Destination";
@@ -10,6 +16,10 @@ const METHOD_ENCODE: &str               = "encode";
 const METHOD_CREATE_CLIENT: &str        = "createClient";
 const METHOD_TO_BASE32: &str            = "toBase32";
 const METHOD_GET_SK: &str               = "getSk";
+const METHOD_OF: &str                   = "of";
+const METHOD_WRITE: &str                = "write";
+const METHOD_DELETE_ON_EXIT: &str       = "deleteOnExit";
+const METHOD_DECODE: &str               = "decode";
 
 /// Keypair contains the secret key `sk`
 ///
@@ -23,7 +33,7 @@ struct KeyPair {
 impl KeyPair {
     fn generate() -> Result<KeyPair, errors::J4RsError> {
         log::info!("Keypair::generate");
-        let jvm = JvmBuilder::new().with_base_path("opt/j4-i2p-rs").build()?;
+        let jvm = JvmBuilder::new().with_base_path(BASE_PATH).build()?;
         let sk_instance = jvm.create_instance(BYTE_ARRAY_STREAM_CLASS, InvocationArg::empty())?;
         let client = jvm.invoke_static(I2P_CLIENT_FACTORY_CLASS, METHOD_CREATE_CLIENT, InvocationArg::empty())?;
         let destination = jvm.invoke(&client, METHOD_CREATE_DESTINATION, &[&InvocationArg::from(sk_instance)])?;
@@ -31,16 +41,43 @@ impl KeyPair {
         let b64 = jvm.invoke_static(BASE64_CLASS, METHOD_ENCODE, &[InvocationArg::from(sk_bytes)])?;
         let sk: String = jvm.to_rust(b64)?;
         let b32_dest_instance = jvm.invoke(&destination, METHOD_TO_BASE32, InvocationArg::empty())?;
-        let b32_dest = jvm.to_rust(b32_dest_instance)?;
+        let b32_dest: String = jvm.to_rust(b32_dest_instance)?;
         Ok(KeyPair {b32_dest, sk})
     }
 }
 
-/// Server tunnels consist of a host, port and Pair
+/// Server tunnels consist of a host, port and KeyPair
 pub struct ServerTunnel {
     host: String,
+    keypair: KeyPair,
     port: u16,
-    keypair: KeyPair
+}
+
+impl ServerTunnel {
+    fn new(keypair: KeyPair, host: String, port: u16) -> Self {
+        ServerTunnel {
+            host,
+            keypair,
+            port
+        }
+    }
+    fn start(&self) -> Result<(), errors::J4RsError> {
+        log::info!("starting tunnel on {}", self.keypair.b32_dest);
+        let jvm = JvmBuilder::new().with_base_path(BASE_PATH).build()?;
+        let mut data = [0u8; 32];
+        rand::thread_rng().fill_bytes(&mut data);
+        let uuid = hex::encode(data);
+        let sk_path = format!("sk.{}.dat", uuid);
+        let path = jvm.invoke_static(PATH_CLASS, METHOD_OF, &[InvocationArg::try_from(&sk_path)?])?;
+        let b64_decode = jvm.invoke_static(BASE64_CLASS, METHOD_DECODE, &[InvocationArg::try_from(self.keypair.b32_dest.clone())?])?;
+        let _ = jvm.invoke_static(FILES_CLASS, METHOD_WRITE, &[InvocationArg::from(path), InvocationArg::from(b64_decode)])?;
+        let file = jvm.create_instance(FILE_CLASS, &[InvocationArg::try_from(&sk_path)?])?;
+        let _ = jvm.invoke(&file, METHOD_DELETE_ON_EXIT, InvocationArg::empty())?;
+        let server_arg = format!("server {} {} {}", self.host, self.port, &sk_path);
+        let args: &[String] = &["-die".to_string(), "-nocli".to_string(), "-e".to_string(), server_arg];
+        let _ = jvm.create_instance(I2P_TUNNEL_CLASS, &[InvocationArg::try_from(args)?])?;
+        Ok(())
+    } 
 }
 
 
